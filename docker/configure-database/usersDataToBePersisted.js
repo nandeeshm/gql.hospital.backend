373361107be9641611411f5d1e81ee36944c038b
ddialar <ddialar@gmail.com>
2019-07-10 23:39:32 +0100

    chore(docker): Update database pre-seeded user password.

--- docker/configure-database/usersDataToBePersisted.js
+++ docker/configure-database/usersDataToBePersisted.js
@@ -3,7 +3,7 @@ const usersDataToBePersisted = [
         'name': 'Dailos',
         'surname': 'Díaz',
         'username': 'ddialar',
-        'password': '$2a$04$up0a0W22m1HBjECz33PVZekXM9lGsWu1/jmyhePkF04wFsaVD5QmS',
+        'password': '$2a$04$YOciD.lG1gON7VHCNef2nuu4AfekO8vm5GHxz2payJy1VC954WAk',
         'role': 29,
         'token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkZGlhbGFyIiwiaWF0IjoxNTEzMjYzMjA5LCJleHAiOjE1MTMyNjY4MDksInJvbGUiOjB9.UieL1uQT-9RVzsLbja-pLSV0KB6pTr6Tsfc7BXZZ8-w',
         'locale': 'es-ES',
